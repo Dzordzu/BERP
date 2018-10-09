bf0a4fe53374c59053f2a17cd71a1fdf9ff3e896
Dzordzu <tomekdur@wp.pl>
2018-10-09 02:37:47 +0200

    All payments work... Maybe not perfectly but work

--- src/main/java/business/payment/BonusPayment.java
+++ src/main/java/business/payment/BonusPayment.java
@@ -1,54 +1,65 @@
 package business.payment;
 
 import valueobj.money.Money;
+import valueobj.money.tax.Assurances;
+import valueobj.money.tax.PensionTax;
+import valueobj.money.tax.Tax;
+import valueobj.money.tax.TestPeriod;
 
 public class BonusPayment implements PaymentStrategy{
 
-    public BonusPayment() {
-    }
+    Money netEmploymentCost;
+    Tax pensionTax = PensionTax.getInstance();
+    Tax assurances = Assurances.getInstance();
+    Tax testPeriod = TestPeriod.getInstance();
 
     @Override
     public String getPaymentName() {
-        return "Bonus Payment";
+        return "Standard Payment";
     }
 
     @Override
     public void setNetEmployeeSalary(Money salary) {
-
+        salary = pensionTax.revertTax(salary);
+        this.netEmploymentCost = testPeriod.applyTax(salary);
     }
 
     @Override
     public void setGrossEmploymentCost(Money cost) {
-
+        this.netEmploymentCost = assurances.revertTax(cost);
     }
 
     @Override
     public Money getNetEmployeeSalary() {
-        return null;
+        return pensionTax.applyTax(
+                testPeriod.revertTax(this.netEmploymentCost)
+        );
     }
 
     @Override
     public Money getGrossEmploymentCost() {
-        return null;
+        return assurances.applyTax(this.netEmploymentCost);
     }
 
     @Override
     public void setNetEmploymentCost(Money cost) {
-
+        this.netEmploymentCost = cost;
     }
 
     @Override
     public void setGrossEmployeeSalary(Money salary) {
-
+        this.netEmploymentCost = salary;
     }
 
     @Override
     public Money getNetEmploymentCost() {
-        return null;
+        return this.netEmploymentCost;
     }
 
     @Override
     public Money getGrossEmployeeSalary() {
-        return null;
+        return this.netEmploymentCost;
     }
+
+
 }
