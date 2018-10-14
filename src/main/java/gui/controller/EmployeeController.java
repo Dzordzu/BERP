8560e23b3811043ff48316ce1048b27f70b7d67b
Dzordzu <tomekdur@wp.pl>
2018-10-14 18:44:33 +0200

    Added cancel method in the employee controller

--- src/main/java/gui/controller/EmployeeController.java
+++ src/main/java/gui/controller/EmployeeController.java
@@ -1,4 +1,15 @@
 package gui.controller;
 
+import gui.SceneSwitcher;
+import gui.helper.BillingTableGenerator;
+
 public class EmployeeController {
+
+    public void save() {
+        System.out.println("TODO");
+    }
+
+    public void cancel() throws Exception {
+        SceneSwitcher.getInstance().switchScene(BillingTableGenerator.getInstance().generate());
+    }
 }
