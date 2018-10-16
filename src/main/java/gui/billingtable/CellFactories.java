31502be95df8603fc21171860c19427860b535f6
Dzordzu <tomekdur@wp.pl>
2018-10-16 23:27:02 +0200

    Minor fixes

--- src/main/java/gui/billingtable/CellFactories.java
+++ src/main/java/gui/billingtable/CellFactories.java
@@ -2,13 +2,12 @@ package gui.billingtable;
 
 import business.Employee;
 import business.EmployeeManager;
+import gui.SceneSwitcher;
 import gui.helper.EmployeeViewGenerator;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
 import lombok.Getter;
-
-import java.io.IOException;
 
 /**
  * Handles all the cell factories
@@ -24,8 +23,8 @@ public enum CellFactories {
     Edit(
             ActionButtonTableCell.<Employee>forTableColumn("Edit", e -> {
                 try {
-                    EmployeeViewGenerator.EDIT_MODE.generate(e);
-                } catch (IOException e1) {
+                    SceneSwitcher.getInstance().switchScene(EmployeeViewGenerator.EDIT_MODE.generate(e));
+                } catch (Exception e1) {
                     e1.printStackTrace();
                 }
                 return e;
@@ -34,8 +33,8 @@ public enum CellFactories {
     View(
             ActionButtonTableCell.<Employee>forTableColumn("View", e -> {
                 try {
-                    EmployeeViewGenerator.VIEW_MODE.generate(e);
-                } catch (IOException e1) {
+                    SceneSwitcher.getInstance().switchScene(EmployeeViewGenerator.VIEW_MODE.generate(e));
+                } catch (Exception e1) {
                     e1.printStackTrace();
                 }
                 return e;
