f01467da96d535f78344c9f63327b4164926dd2b
Dzordzu <tomekdur@wp.pl>
2018-10-16 21:55:15 +0200

    CellValuefactories are (hopefully) ready

--- src/main/java/gui/billingtable/CellValueFactories.java
+++ src/main/java/gui/billingtable/CellValueFactories.java
@@ -1,17 +1,65 @@
 package gui.billingtable;
 
+import business.Employee;
+import business.payment.PaymentStrategy;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+import logic.math.Percentage;
+import lombok.Getter;
+
 /**
  * Handles all the CellValueFactories
  */
 
 public enum CellValueFactories {
-    ID,
-    Name,
-    Position,
-    PaymentStrategy,
-    NetEmployeeSalary,
-    GrossEmploymentCost,
-    Currency,
-    NESGEC,
+    ID(
+            (Callback<TableColumn.CellDataFeatures<Employee, String>, ObservableValue<String>>) param ->
+                    new SimpleStringProperty(param.getValue().getId().getValue())
+
+    ),
+    Name(
+            (Callback<TableColumn.CellDataFeatures<Employee, String>, ObservableValue<String>>) param ->
+                    new SimpleStringProperty(param.getValue().getPerson().getName().toString())
+
+    ),
+    JobTitle(
+            (Callback<TableColumn.CellDataFeatures<Employee, String>, ObservableValue<String>>) param ->
+                    new SimpleStringProperty(param.getValue().getJob().getJobTitle())
+
+    ),
+    PaymentStrategy(
+            (Callback<TableColumn.CellDataFeatures<Employee, String>, ObservableValue<String>>) param ->
+                    new SimpleStringProperty(param.getValue().getJob().getSalary().getPaymentName())
+    ),
+    NetEmployeeSalary(
+            (Callback<TableColumn.CellDataFeatures<Employee, Double>, ObservableValue<Double>>) param ->
+                    new SimpleDoubleProperty(param.getValue().getJob().getSalary().getNetEmployeeSalary().getAmount()).asObject()
+    ),
+    GrossEmploymentCost(
+            (Callback<TableColumn.CellDataFeatures<Employee, Double>, ObservableValue<Double>>) param ->
+                    new SimpleDoubleProperty(param.getValue().getJob().getSalary().getGrossEmploymentCost().getAmount()).asObject()
+    ),
+    Currency(
+            (Callback<TableColumn.CellDataFeatures<Employee, String>, ObservableValue<String>>) param ->
+                    new SimpleStringProperty(param.getValue().getJob().getSalary().getGrossEmploymentCost().getCurrency().getCurrencyCode())
+    ),
+    NESGEC(
+            (Callback<TableColumn.CellDataFeatures<Employee, Double>, ObservableValue<Double>>) param -> {
+                PaymentStrategy strategy = param.getValue().getJob().getSalary();
+                double NES = strategy.getNetEmployeeSalary().getAmount();
+                double GEC = strategy.getGrossEmployeeSalary().getAmount();
+                Percentage result = new Percentage().setFraction(NES/GEC);
+                return new SimpleDoubleProperty(result.getPercentage()).asObject();
+            }
+    ),
     ;
+
+    @Getter Callback<TableColumn.CellDataFeatures, ObservableValue> value;
+
+    CellValueFactories(Callback callback) {
+        this.value = callback;
+    }
 }
