440bc74eee206e850286ae2dfae2b1c71d4f737f
Dzordzu <tomekdur@wp.pl>
2018-10-16 14:27:21 +0200

    Updated NameBuilder

--- src/main/java/logic/human/NameBuilder.java
+++ src/main/java/logic/human/NameBuilder.java
@@ -2,8 +2,8 @@ package logic.human;
 
 
 import logic.Builder;
+import lombok.Setter;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class NameBuilder implements Builder<Name> {
@@ -13,29 +13,10 @@ public class NameBuilder implements Builder<Name> {
 	}
 	private NameBuilder() {}
 
-	protected String firstname = null;
-	protected String surname = null;
-	protected List<String> middlenames = null;
-	protected List<String> suffixes = null;
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	public void setMiddlenames(List<String> middlenames) {
-		this.middlenames = middlenames;
-	}
-	public void setMiddlenames(String[] middlenames) {
-		this.middlenames = Arrays.asList(middlenames);
-	}
-	public void setSuffixes(List<String> suffixes) {
-		this.suffixes = suffixes;
-	}
-	public void setSuffixes(String[] suffixes) {
-		this.suffixes = Arrays.asList(suffixes);
-	}
+	@Setter protected String firstname = null;
+	@Setter protected String surname = null;
+	@Setter protected List<String> middlenames = null;
+	@Setter protected List<String> suffixes = null;
 
 	public void clear() {
 		firstname = null;
@@ -43,7 +24,6 @@ public class NameBuilder implements Builder<Name> {
 		middlenames = null;
 		suffixes = null;
 	}
-
 
 	public Name build() {
 		return new Name(firstname, surname, middlenames, suffixes);
