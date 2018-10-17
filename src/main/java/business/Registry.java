af2e6cd629e2801d2b96a4ab4f768e0477051f73
Dzordzu <tomekdur@wp.pl>
2018-10-17 03:42:10 +0200

    Created registry interface. Created registries

---
+++ src/main/java/business/Registry.java
@@ -0,0 +1,11 @@
+package business;
+
+/**
+ *  <p>Registry stores info about (registered) interfaces implementations</p>
+ *  <p>
+ *      Registry has to be an enum. Each registry entry has to be also a name of the single implementation.
+ *      You can also set display name of the implementation.
+ *  </p>
+ */
+
+public interface Registry {}
---
+++ src/main/java/business/payment/PaymentsRegistry.java
@@ -0,0 +1,31 @@
+package business.payment;
+
+import business.Registry;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Place where all payments have to be registered
+ */
+
+public enum PaymentsRegistry implements Registry {
+    StandardPayment,
+    BonusPayment,
+    TestPeriodPayment
+    ;
+
+    PaymentsRegistry(){}
+
+    public Registry getDefault() {
+        return PaymentsRegistry.StandardPayment;
+    }
+
+    public String toString() {
+        String result;
+        Pattern p = Pattern.compile("[A-Z][a-z]+");
+        Matcher m = p.matcher(this.name());
+        result = m.replaceAll("$1 ");
+        return result.substring(0, result.length()-1);
+    }
+}
