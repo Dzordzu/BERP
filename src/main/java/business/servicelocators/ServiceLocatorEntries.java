8437de64dfe08436c9bf009097e1c0c5cc0ceec1
Dzordzu <tomekdur@wp.pl>
2018-10-22 19:41:36 +0200

    Added comments

--- src/main/java/business/servicelocators/ServiceLocatorEntries.java
+++ src/main/java/business/servicelocators/ServiceLocatorEntries.java
@@ -1,7 +1,29 @@
 package business.servicelocators;
 
+
+/**
+ * Dedicated for enums or lists handling info about registered services
+ */
 public interface ServiceLocatorEntries {
+    /**
+     * UI name is the main identifier of the service within GUI, API etc.
+     * @return UIName
+     */
     String getUIName();
+
+    /**
+     * UI name is the main identifier of the service within GUI, API etc.
+     * @return UIName
+     */
     String getServiceName();
+
+    /**
+     * Class reference can be used to generate new instances of the class service.
+     * <br />Example :
+     * <code>
+     * serviceLocatorEntryInstance.getClassRef().getConstructor().newInstance();
+     *  </code>
+     * @return UIName
+     */
     Class getClassRef();
 }
