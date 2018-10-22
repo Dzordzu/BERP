be1aaf7970b04d03a6f958916385b272da14b1bc
Dzordzu <tomekdur@wp.pl>
2018-10-22 18:07:36 +0200

    Added new Locator

---
+++ src/main/java/business/payment/PaymentsServiceLocator.java
@@ -0,0 +1,60 @@
+package business.payment;
+
+import business.ServiceLocator;
+import business.ServiceLocatorEntries;
+import lombok.Getter;
+
+public class PaymentsServiceLocator implements ServiceLocator {
+    private static PaymentsServiceLocator ourInstance = new PaymentsServiceLocator();
+    public static PaymentsServiceLocator getInstance() {
+        return ourInstance;
+    }
+    private PaymentsServiceLocator() {}
+
+    enum VALUES implements ServiceLocatorEntries {
+        StandardPayment("Standard Payment", "StandardPayment", StandardPayment.class),
+        TestPeriodPayment("Test Period Payment", "TestPeriodPayment", TestPeriodPayment.class),
+        BonusPayment("Bonus Payment", "BonusPayment", BonusPayment.class),
+        ;
+
+        @Getter String UIName;
+        @Getter String serviceName;
+        @Getter Class classRef;
+
+        VALUES(String UIName, String serviceName, Class classRef) {
+            this.UIName = UIName;
+            this.serviceName = serviceName;
+            this.classRef = classRef;
+        }
+    }
+
+    VALUES defaultValue = VALUES.StandardPayment;
+
+    @Override
+    public ServiceLocatorEntries[] getValues() {
+        return VALUES.values();
+    }
+
+    @Override
+    public ServiceLocatorEntries getDefault() {
+        return defaultValue;
+    }
+
+    @Override
+    public Boolean exists(String UIName) {
+        for(VALUES v: VALUES.values()) {
+            if(v.getUIName() == UIName) return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public ServiceLocatorEntries getMatching(String UIName) {
+        for(VALUES v: VALUES.values()) {
+            if(v.getUIName() == UIName) return v;
+        }
+
+        return null;
+    }
+}
