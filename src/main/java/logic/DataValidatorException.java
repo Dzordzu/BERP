fb5bd3fb95d96bff205eb1b33e118a2933c32a31
Dzordzu <tomekdur@wp.pl>
2018-10-23 02:12:04 +0200

    Added comments

--- src/main/java/logic/DataValidatorException.java
+++ src/main/java/logic/DataValidatorException.java
@@ -1,5 +1,8 @@
 package logic;
 
+/**
+ * Is thrown when data is improper (ex. numeric when the requirements specify alphabetical)
+ */
 public class DataValidatorException extends Exception {
     public DataValidatorException(String message) {
         super(message);
